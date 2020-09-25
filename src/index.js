000568a90e7ec506e9155cb390bd7fc3d390aafe
Rickard Börjesson (bwf665) <rickard.borjesson@teliacompany.com>
2020-09-26 01:59:09 +0200

    Change api to make schema recursive

--- src/index.js
+++ src/index.js
@@ -1,51 +1,60 @@
+import { curry } from './utils';
+
 const defaultOptions = {
     parseObject: false,
-    strict: false
+    strict: false,
+    name: null,
+    notNil: false,
+    notEmpty: false
 };
 
-export default class {
+class JsObjectSchema {
     constructor(schema, options = {}) {
         this.schema = schema || {};
         this.options = { ...defaultOptions, ...options };
+        this._createValidationErrors = curry((schema, object, validationErrors, key) => {
+            const schemaNode = schema[key];
+            const value = object ? object[key] : null;
+
+            if (typeof schemaNode === 'function') {
+                const errorSuffix = `Error in ${this.options.name || 'Root Object'}: `;
+                return schemaNode(object || {})
+                    ? validationErrors
+                    : [ ...validationErrors, Error(`${errorSuffix}${key} is invalid.`) ];
+            }
+
+            if (typeof schemaNode === 'object' && schemaNode !== null && !Array.isArray(schemaNode)) {
+                return this._handleNonFunctionSchemaNode(schemaNode, key, validationErrors, object[key]);
+            }
+
+            if (Array.isArray(schemaNode)) {
+                return value.reduce(this._handleNonFunctionSchemaNode(schemaNode[0], key), validationErrors);
+            }
+
+            throw new Error(`Error for key '${key}': Handler must be a function or JsObjectSchema`);
+        });
+
+        this._handleNonFunctionSchemaNode = curry((schemaNode, key, validationErrors, obj) => {
+            const schemaObj = schemaNode instanceof JsObjectSchema
+                ? schemaNode
+                : new JsObjectSchema(schemaNode, { name: key });
+
+            const { error } = schemaObj.validate(obj, key);
+            return error ? [ ...validationErrors, ...error.errors ] : validationErrors;
+        });
+
         return this;
     }
 
-    validate(object) {
-        const executeValidationOnKey = (acc, key) => {
-            const handleValidation = (errors, validationNode) => {
-                if (typeof validationNode === 'object' && validationNode !== null) {
-                    if (typeof validationNode.handler !== 'function') {
-                        throw new Error(`Error for key '${key}': Handler must be a function`);
-                    }
-
-                    return validationNode.handler(object || {})
-                        ? errors
-                        : [ ...errors, new Error(validationNode.errorMessage || `${key} is invalid.`) ];
-                }
-
-                if (typeof validationNode === 'function') {
-                    return validationNode(object || {}) ? errors : [ ...errors, new Error(`${key} is invalid.`) ];
-                }
-
-                throw new Error(`Error for key '${key}': Handler must be a function`);
-            };
-
-            const schemaKey = this.schema[key];
-            if (Array.isArray(schemaKey)) {
-                return schemaKey.reduce(handleValidation, acc);
-            } else {
-                return handleValidation(acc, schemaKey);
-            }
-        };
-
-        const strictError = this.options.strict ? this._checkStrict(Object) : null;
+    validate(object, key = this.options.name) {
+        const strictError = this._getOptionalError(Object, key);
         if (strictError) {
             return strictError;
         }
 
         const validationErrors = Object
             .keys(this.schema)
-            .reduce(executeValidationOnKey, []);
+            .reduce(this._createValidationErrors(this.schema, object), []);
 
         return {
             object: this.options.parseObject ? this._parseObject(object) : object,
@@ -58,16 +67,24 @@ export default class {
             .reduce((acc, key) => ({ ...acc, [key]: object[key] }), {});
     }
 
-    _checkStrict(object) {
-        const schemaKeys = Object.keys(this.schema).sort();
-        const objectKeys = Object.keys(object).sort();
+    _getOptionalError(object, key) {
+        const message = 'Schema validation error';
+        const objName = key || 'Root Object';
 
-        return JSON.stringify(objectKeys) === JSON.stringify(schemaKeys) ? null : {
-            object,
-            error: {
-                message: 'Strict violation error',
-                errors: [ new Error('Object include properties that is not in schema') ]
-            }
-        };
+        if (this.options.notNil && !object) {
+            return { object, error: { message, errors: [ new Error(`${objName} must not be nil.`) ] } };
+        }
+
+        if (this.options.notEmpty && (!object || !Object.keys(object).length)) {
+            return { object, error: { message, errors: [ new Error(`${objName} must not be empty.`) ] } };
+        }
+
+        if (this.options.strict && JSON.stringify(Object.keys(this.schema).sort()) !== JSON.stringify(Object.keys(object).sort())) {
+            return { object, error: { message, errors: [ new Error(`${objName} include properties that is not in schema`) ] } };
+        }
+
+        return null;
     }
-};
+}
+
+export default JsObjectSchema;
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,6 +1,10 @@
 import JsObjectSchema from '../src/index';
 
-describe('schema', () => {
+const squirtle = { name: 'Squirtle', level: 5, maxLevel: 99 };
+const bulbasaur = { name: 'Bulbasaur', level: 5, maxLevel: 99 };
+const picachu = { name: 'Picachu', level: 99, maxLevel: 99 };
+
+describe('JsObjectSchema', () => {
     test('plain schema given valid object', () => {
         const schema = new JsObjectSchema({
             name: ({ name }) => typeof name === 'string',
@@ -8,62 +12,10 @@ describe('schema', () => {
             level: ({ level, maxLevel }) => typeof level === 'number' && level <= maxLevel
         });
 
-        const object = {
-            name: 'Picachu',
-            level: 99,
-            maxLevel: 99
-        };
+        const result = schema.validate(picachu);
 
-        const result = schema.validate(object);
-
-        expect(result.object).toEqual(object);
         expect(result.error).toBe(null);
-    });
-
-    test('schema with handler and custom message given invalid object', () => {
-        const schema = new JsObjectSchema({
-            name: ({ name }) => typeof name === 'string',
-            maxLevel: {
-                handler: ({ maxLevel }) => typeof maxLevel === 'number' && maxLevel > 0,
-                errorMessage: 'maxLevel must be a number greater than 0.'
-            },
-            level: ({ level, maxLevel }) => typeof level === 'number' && level <= maxLevel
-        });
-
-        const object = { name: 'Charmander', level: 0, maxLevel: 0 };
-
-        const result = schema.validate(object);
-
-        expect(result.object).toEqual(object);
-        expect(result.error.errors.length).toBe(1);
-        expect(result.error.message).toBe('Schema validation error');
-        expect(result.error.errors[0].message).toBe('maxLevel must be a number greater than 0.');
-    });
-
-    test('schema with arrays with mixed values given invalid object', () => {
-        const schema = new JsObjectSchema({
-            name: ({ name }) => typeof name === 'string',
-            maxLevel: {
-                handler: ({ maxLevel }) => typeof maxLevel === 'number' && maxLevel > 0,
-                errorMessage: 'maxLevel must be a number greater than 0.'
-            },
-            level: [
-                ({ level }) => typeof level === 'number',
-                {
-                    handler: ({ level, maxLevel }) => typeof level === 'number' && level <= maxLevel,
-                    errorMessage: 'level must be lower or equal to maxLevel'
-                }
-            ]
-        });
-
-        const object = { name: 'Squirtle', level: 99, maxLevel: 50 };
-
-        const result = schema.validate(object);
-
-        expect(result.object).toEqual(object);
-        expect(result.error.errors.length).toBe(1);
-        expect(result.error.message).toBe('Schema validation error');
-        expect(result.error.errors[0].message).toBe('level must be lower or equal to maxLevel');
+        expect(result.object).toEqual(picachu);
     });
 
     test('schema given options with parseObject true should only include props from schema', () => {
@@ -73,12 +25,11 @@ describe('schema', () => {
             level: ({ level, maxLevel }) => typeof level === 'number' && level <= maxLevel
         }, { parseObject: true });
 
-        const baseObject = { name: 'Squirtle', level: 1, maxLevel: 99 };
-        const objectWithExtraProps = { ...baseObject, hp: 60, attacks: [ 'Tackle', 'Leer' ] };
+        const objectWithExtraProps = { ...squirtle, hp: 60, attacks: [ 'Tackle', 'Leer' ] };
 
         const result = schema.validate(objectWithExtraProps);
 
-        expect(result.object).toEqual(baseObject);
+        expect(result.object).toEqual(squirtle);
     });
 
     test('schema given options with strict true should return specific error when object has keys doesn\'t match schema', () => {
@@ -86,13 +37,187 @@ describe('schema', () => {
             name: ({ name }) => typeof name === 'string',
             maxLevel: ({ maxLevel }) => typeof maxLevel === 'number' && maxLevel > 0,
             level: ({ level, maxLevel }) => typeof level === 'number' && level <= maxLevel
-        }, { strict: true });
+        }, { strict: true, name: 'Pokemon' });
 
-        const object = { name: 'Squirtle', level: 1, maxLevel: 99, hp: 60, attacks: [ 'Tackle', 'Leer' ] };
+        const object = { ...squirtle, hp: 60, attacks: [ 'Tackle', 'Leer' ] };
         const result = schema.validate(object);
 
         expect(result.error.errors.length).toBe(1);
-        expect(result.error.message).toBe('Strict violation error');
-        expect(result.error.errors[0].message).toBe('Object include properties that is not in schema');
+        expect(result.error.message).toBe('Schema validation error');
+        expect(result.error.errors[0].message).toBe('Pokemon include properties that is not in schema');
+    });
+
+    test('schema with prop that is a schema given valid object', () => {
+        const pokemonSchema = new JsObjectSchema({
+            name: ({ name }) => typeof name === 'string',
+            maxLevel: ({ maxLevel }) => typeof maxLevel === 'number' && maxLevel > 0,
+            level: ({ level, maxLevel }) => typeof level === 'number' && level <= maxLevel
+        }, { name: 'pokemon' });
+
+        const trainerSchema = new JsObjectSchema({
+            name: ({ name }) => typeof name === 'string',
+            badges: ({ badges }) => Array.isArray(badges),
+            pokemon: pokemonSchema
+        });
+
+        const trainer = { name: 'Ash', badges: [], pokemon: squirtle };
+
+        const result = trainerSchema.validate(trainer);
+
+        expect(result.error).toBe(null);
+        expect(result.object).toEqual(trainer);
+    });
+
+    test('schema with prop that is a schema given invalid object', () => {
+        const pokemonSchema = new JsObjectSchema({
+            name: ({ name }) => typeof name === 'string',
+            maxLevel: ({ maxLevel }) => typeof maxLevel === 'number' && maxLevel > 0,
+            level: ({ level, maxLevel }) => typeof level === 'number' && level <= maxLevel
+        }, { name: 'Pokemon' });
+
+        const trainerSchema = new JsObjectSchema({
+            name: ({ name }) => typeof name === 'string',
+            badges: ({ badges }) => Array.isArray(badges),
+            pokemon: pokemonSchema
+        });
+
+        const trainer = { name: 'Ash', badges: null, pokemon: { maxLevel: 0, level: 1, name: 'missingno' } };
+
+        const result = trainerSchema.validate(trainer);
+
+        expect(result.error.errors.length).toBe(3);
+        expect(result.error.message).toBe('Schema validation error');
+        expect(result.error.errors[0].message).toBe('Error in Root Object: badges is invalid.');
+        expect(result.error.errors[1].message).toBe('Error in Pokemon: maxLevel is invalid.');
+        expect(result.error.errors[2].message).toBe('Error in Pokemon: level is invalid.');
+    });
+
+    test('schema with an object as prop given valid object', () => {
+        const trainerSchema = new JsObjectSchema({
+            name: ({ name }) => typeof name === 'string',
+            badges: ({ badges }) => Array.isArray(badges),
+            pokemon: {
+                name: ({ name }) => typeof name === 'string',
+                maxLevel: ({ maxLevel }) => typeof maxLevel === 'number' && maxLevel > 0,
+                level: ({ level, maxLevel }) => typeof level === 'number' && level <= maxLevel
+            }
+        });
+
+        const trainer = { name: 'Ash', badges: [], pokemon: bulbasaur };
+
+        const result = trainerSchema.validate(trainer);
+
+        expect(result.error).toBe(null);
+        expect(result.object).toEqual(trainer);
+    });
+
+    test('schema with an object as prop given invalid object', () => {
+        const trainerSchema = new JsObjectSchema({
+            name: ({ name }) => typeof name === 'string',
+            badges: ({ badges }) => Array.isArray(badges),
+            pokemon: {
+                name: ({ name }) => typeof name === 'string',
+                maxLevel: ({ maxLevel }) => typeof maxLevel === 'number' && maxLevel > 0,
+                level: ({ level, maxLevel }) => typeof level === 'number' && level <= maxLevel
+            }
+        });
+
+        const trainer = { name: 'Ash', badges: [], pokemon: { name: null, maxLevel: 99, level: 5 } };
+
+        const result = trainerSchema.validate(trainer);
+
+        expect(result.error.errors.length).toBe(1);
+        expect(result.error.message).toBe('Schema validation error');
+        expect(result.error.errors[0].message).toBe('Error in pokemon: name is invalid.');
+    });
+
+    test('schema with prop that is an array with schema given valid object', () => {
+        const pokemonSchema = new JsObjectSchema({
+            name: ({ name }) => typeof name === 'string',
+            maxLevel: ({ maxLevel }) => typeof maxLevel === 'number' && maxLevel > 0,
+            level: ({ level, maxLevel }) => typeof level === 'number' && level <= maxLevel
+        }, { name: 'Pokemon' });
+
+        const trainerSchema = new JsObjectSchema({
+            name: ({ name }) => typeof name === 'string',
+            badges: ({ badges }) => Array.isArray(badges),
+            pokemon: [ pokemonSchema ]
+        });
+
+        const trainer = { name: 'Ash', badges: [], pokemon: [ bulbasaur, squirtle ] };
+        const result = trainerSchema.validate(trainer);
+
+        expect(result.error).toBe(null);
+        expect(result.object).toEqual(trainer);
+    });
+
+    test('schema with prop that is an array with schema given invalid object', () => {
+        const pokemonSchema = new JsObjectSchema({
+            name: ({ name }) => typeof name === 'string',
+            maxLevel: ({ maxLevel }) => typeof maxLevel === 'number' && maxLevel > 0,
+            level: ({ level, maxLevel }) => typeof level === 'number' && level <= maxLevel
+        }, { name: 'Pokemon' });
+
+        const trainerSchema = new JsObjectSchema({
+            name: ({ name }) => typeof name === 'string',
+            badges: ({ badges }) => Array.isArray(badges),
+            pokemon: [ pokemonSchema ]
+        }, { name: 'Trainer' });
+
+        const trainer = {
+            name: null,
+            badges: [ 'Pewter Gym' ],
+            pokemon: [ bulbasaur, squirtle, { name: null, level: 40, maxLevel: 40 } ]
+        };
+
+        const result = trainerSchema.validate(trainer);
+
+        expect(result.error.errors.length).toBe(2);
+        expect(result.error.message).toBe('Schema validation error');
+        expect(result.error.errors[0].message).toBe('Error in Trainer: name is invalid.');
+        expect(result.error.errors[1].message).toBe('Error in Pokemon: name is invalid.');
+    });
+
+    test('schema with prop that is an array with object given valid object', () => {
+        const trainerSchema = new JsObjectSchema({
+            name: ({ name }) => typeof name === 'string',
+            badges: ({ badges }) => Array.isArray(badges),
+            pokemon: [{
+                name: ({ name }) => typeof name === 'string',
+                maxLevel: ({ maxLevel }) => typeof maxLevel === 'number' && maxLevel > 0,
+                level: ({ level, maxLevel }) => typeof level === 'number' && level <= maxLevel
+            }]
+        });
+
+        const trainer = { name: 'Ash', badges: [], pokemon: [ bulbasaur, squirtle ] };
+
+        const result = trainerSchema.validate(trainer);
+
+        expect(result.error).toBe(null);
+        expect(result.object).toEqual(trainer);
+    });
+
+    test('schema with prop that is an array with object given invalid object', () => {
+        const trainerSchema = new JsObjectSchema({
+            name: ({ name }) => typeof name === 'string',
+            badges: ({ badges }) => Array.isArray(badges),
+            pokemon: [{
+                name: ({ name }) => typeof name === 'string',
+                maxLevel: ({ maxLevel }) => typeof maxLevel === 'number' && maxLevel > 0,
+                level: ({ level, maxLevel }) => typeof level === 'number' && level <= maxLevel
+            }]
+        });
+
+        const trainer = {
+            name: 'Ash',
+            badges: [],
+            pokemon: [ { name: 'Rattata', level: 41, maxLevel: 40 }, bulbasaur, squirtle ]
+        };
+
+        const result = trainerSchema.validate(trainer);
+
+        expect(result.error.errors.length).toBe(1);
+        expect(result.error.message).toBe('Schema validation error');
+        expect(result.error.errors[0].message).toBe('Error in pokemon: level is invalid.');
     });
 });
